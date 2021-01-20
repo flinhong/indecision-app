ceeb2f2339e2acdab62a6f69f363252a482add09
Frank Lin <flinhong@hotmail.com>
2021-01-21 00:06:07 +0800

    update webpack for production

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,33 @@
 const path = require('path');
 // entry -> output
-module.exports = {
-  entry: './src/app.js',
-  output: {
-    path: path.join(__dirname, 'public'),
-    filename: 'bundle.js'
-  },
-  module: {
-    rules: [{
-      loader: 'babel-loader',
-      test: /\.js$/, // file ends with .js
-      exclude: /node_modules/
-    }, {
-      test: /\.s?css$/,
-      use: [
-        'style-loader',
-        'css-loader',
-        'sass-loader'
-      ]
-    }]
-  },
-  devtool: 'eval-cheap-module-source-map',
-  devServer: {
-    contentBase: path.join(__dirname, 'public')
-  },
-  mode: 'development'
-};
+module.exports = (env) => {
+  console.log(env);
+  return {
+    entry: './src/app.js',
+    output: {
+      path: path.join(__dirname, 'public'),
+      filename: 'bundle.js'
+    },
+    module: {
+      rules: [{
+        loader: 'babel-loader',
+        test: /\.js$/, // file ends with .js
+        exclude: /node_modules/
+      }, {
+        test: /\.s?css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      }]
+    },
+    // devtool: 'eval-cheap-module-source-map',
+    devtool: 'source-map',
+    devServer: {
+      contentBase: path.join(__dirname, 'public')
+    },
+    // mode: 'development'
+    mode: 'production'
+  }
+}
